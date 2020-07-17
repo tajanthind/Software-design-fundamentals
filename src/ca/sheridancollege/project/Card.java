a7e12471c0a43bc6b7d41554e36db25e5ef06dfb
user <user@LAPTOP-MO3LOTSK>
2020-07-16 20:02:14 -0400

    deliverable2

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -1,26 +1,24 @@
-/**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
- */
+
 package ca.sheridancollege.project;
 
-/**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
- * @author dancye, 2018
- */
+
 public abstract class Card 
 {
-    //default modifier for child classes
-    
-    /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
-    
-    @Override
-    public abstract String toString();
-    
+    private String color;
+	private String num;
+	public Card(String color, String num) {
+		this.num = num;
+		this.color = color;
+	}
+	public String getColor() {
+		return color;
+	}
+	public String getNum() {
+		return num;
+	}
+	@Override
+	public String toString() {
+		return this.color +" "+ this.num ;
+	}
+
 }
--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * child of this class and instantiate the methods given.
  * @author dancye, 2018
  */
-public abstract class Game 
+public abstract class Game implements DiscardPile, Turn, Mechanics
 {
     private final String gameName;//the title of the game
     private ArrayList <Player> players;// the players of the game
